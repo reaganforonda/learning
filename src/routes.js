21a4597d2fa0bb4cade6cb69cb966c86f877bb2e
reaganforonda <foronda.reagan@gmail.com>
2018-10-03 17:16:43 -0600

    create login view and add to route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LandingView from './components/Landing/LandingView';
 import RegisterView from './components/Register/RegisterView';
+import LoginView from './components/Login/LoginView';
 
 export default (
     <Switch>
         <Route exact path='/' component={LandingView}/>
-        <Route pather='/register' component={RegisterView}/>
+        <Route path='/register' component={RegisterView}/>
+        <Route path='/login' component={LoginView}/>
     </Switch>
 )
