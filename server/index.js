62ffe872b6586965bd8c010555b406fe757bb78c
reaganforonda <foronda.reagan@gmail.com>
2018-10-03 15:25:44 -0600

    setup server index.js file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const app = express();
+const massive = require("massive");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const {SERVER_PORT} = process.env
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/../build`));
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Creeping on Port: ${SERVER_PORT}`);
+});
