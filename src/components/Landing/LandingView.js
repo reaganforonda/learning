8ec828ed761cda1a7ce43c906f736e7fb499e429
reaganforonda <foronda.reagan@gmail.com>
2018-10-03 16:09:12 -0600

    added landing view

---
+++ src/components/Landing/LandingView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class LandingView extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render() {
+        return (
+            <div className='landingView'>
+                Landing View
+            </div>
+        )
+    }
+}
