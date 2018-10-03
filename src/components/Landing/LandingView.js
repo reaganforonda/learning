146274a1176ab8fd926f915b307fb144d3137c71
reaganforonda <foronda.reagan@gmail.com>
2018-10-03 17:33:59 -0600

    add button login

--- src/components/Landing/LandingView.js
+++ src/components/Landing/LandingView.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
-export default class LandingView extends React.Component{
+export class LandingView extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state={}
+        this.state={
+            toLogin: false,
+            toRegister: false
+        }
+
+        this.handlButton = this.handlButton.bind(this);
+    }
+
+    handlButton(e, path) {
+        if(path === 'login') {
+            e.preventDefault();
+            this.props.history.push('/login');
+        } else if (path === 'register') {
+            e.preventDefault();
+            this.props.history.push('/register');
+        }
     }
 
     render() {
         return (
             <div className='landingView'>
-                <button>Login</button>
-                <button>Create Account</button>
+                <button onClick={(e)=>this.handlButton(e, 'login')}>Login</button>
+                <button onClick={(e)=>this.handlButton(e, 'register')}>Create Account</button>
             </div>
         )
     }
 }
+
+export default withRouter(LandingView);
