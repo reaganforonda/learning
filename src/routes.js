2507f453b06d0acd91ac7e369567206d0b6a7660
reaganforonda <foronda.reagan@gmail.com>
2018-10-03 17:15:12 -0600

    add registerview to route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LandingView from './components/Landing/LandingView';
-
+import RegisterView from './components/Register/RegisterView';
 
 export default (
     <Switch>
         <Route exact path='/' component={LandingView}/>
+        <Route pather='/register' component={RegisterView}/>
     </Switch>
 )
