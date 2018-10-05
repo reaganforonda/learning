8bc07da5f3075e9f4c0ed5e658e7d111d614e670
reaganforonda <foronda.reagan@gmail.com>
2018-10-05 14:38:38 -0600

    add login view logic

--- src/components/Login/LoginView.js
+++ src/components/Login/LoginView.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class LoginView extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state={}
+        this.state={
+            email: '',
+            pw: '',
+            displayPWError: false
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handleFormSubmit(e){
+        e.preventDefault();
+        let user = {
+            email: this.state.email.toLowerCase(),
+            pw : this.state.pw
+        }
+
+        axios.post('/api/auth/login', user).then(()=> {
+
+        }).catch((err) => {
+            
+        })
     }
 
     render(){
         return (
             <div className='loginview'>
-                Login View
+                <form className='loginform' onSubmit={(e)=>this.handleFormSubmit(e)}>
+                    <input type='email' required={true} placeholder='Email' name='email' onChange={(e)=>this.handleInputChange(e)}/>
+                    <input type='password' required={true} placeholder='Password' name='pw' onChange={(e)=>this.handleInputChange(e)}/>
+                    {
+                        this.state.displayPWError ? <div>Incorrect Password or Email</div> : null
+                    }
+                    <button>Log In</button>
+                </form>
             </div>
         )
     }
