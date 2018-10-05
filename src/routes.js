54f0690954b5f16edfea7413fbb73e6afae66158
reaganforonda <foronda.reagan@gmail.com>
2018-10-05 13:38:06 -0600

    added dashboard view to routes

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom';
 import LandingView from './components/Landing/LandingView';
 import RegisterView from './components/Register/RegisterView';
 import LoginView from './components/Login/LoginView';
+import DashboardView from './components/Dashboard/DashboardView';
 
 export default (
     <Switch>
         <Route exact path='/' component={LandingView}/>
         <Route path='/register' component={RegisterView}/>
         <Route path='/login' component={LoginView}/>
+        <Route path='/dashboard' component={DashboardView}/>
     </Switch>
 )
