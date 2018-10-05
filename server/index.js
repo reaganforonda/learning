0e6d2a23708138f2cb97330b3b1258ae01f2ff35
reaganforonda <foronda.reagan@gmail.com>
2018-10-04 22:07:29 -0600

    added sessions

--- server/index.js
+++ server/index.js
@@ -4,19 +4,32 @@ const cors = require("cors");
 const app = express();
 const massive = require("massive");
 const dotenv = require("dotenv");
+const session = require('express-session');
+const sessionMiddleware = require('./middlewares/sessionMiddleware');
 dotenv.config();
 const authController = require('./controllers/authController');
 
-const {SERVER_PORT, CONNECTION_STRING} = process.env
+const {SERVER_PORT, CONNECTION_STRING, SECRET_SESSION} = process.env
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(`${__dirname}/../build`));
 
+
 massive(CONNECTION_STRING).then(dbInstance => {
     app.set("db", dbInstance);
 }).catch(e => console.log(`Error: ${e}`));
 
+
+app.use(
+    session({
+        secret: SECRET_SESSION,
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+app.use(sessionMiddleware.checkSession);
 
 // End Points
 app.get('/api/auth/me', authController.validate)
