2ebee87c1d654ec86f5f7f8e13310cd05d6cdefa
reaganforonda <foronda.reagan@gmail.com>
2018-10-04 19:20:31 -0600

    endpoints

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const massive = require("massive");
 const dotenv = require("dotenv");
 dotenv.config();
+const authController = require('./controllers/authController');
 
 const {SERVER_PORT, CONNECTION_STRING} = process.env
 
@@ -15,6 +16,13 @@ app.use(express.static(`${__dirname}/../build`));
 massive(CONNECTION_STRING).then(dbInstance => {
     app.set("db", dbInstance);
 }).catch(e => console.log(`Error: ${e}`));
+
+
+// End Points
+app.get('/api/auth/me', authController.validate)
+app.post('/api/auth/login', authController.login);
+app.get('/api/auth/logout', authController.logout);
+app.post('/api/auth/register', authController.register);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Creeping on Port: ${SERVER_PORT}`);
