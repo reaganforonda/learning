c7baee00a7ff36fe4e3e286bad3a34f560cdff12
reaganforonda <foronda.reagan@gmail.com>
2018-10-04 21:19:02 -0600

    register function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,9 +4,39 @@ const bcrypt = require('bcryptjs');
 module.exports = {
     register : async (req, res) => {
         const db = req.app.get('db');
+        const {email, pw, confirmPW, firstName, lastName} = req.body;
+
+        if(pw !== confirmPW) {
+            res.sendStatus(400);
+        };
+        
+        db.CHECK_EMAIL([email.toLowerCase()]).then((users) => {
+            if(users.length !== 0) {
+                if(users[0].email === email) {
+                    res.status(400).send('Please Login');
+                }
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(confirmPW, salt);
+
+                db.CREATE_USER([email.toLowerCase(), hash, firstName.toLowerCase(), lastName.toLowerCase()]).then((result) => {
+                    res.sendStatus(200);
+                }).catch((err) => {
+                    console.log(`Server error while attempting to create a new user: ${err}`);
+                });
+            }
+        })
     },
 
     login: (req, res) => {
         const db = req.app.get('db');
+    },
+
+    validate: (req, res) => {
+
+    },
+
+    logout: (req, res) => {
+
     }
 }
