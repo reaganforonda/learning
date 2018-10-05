458f407f7d6126d8ebf197df3f463713cb4f549d
reaganforonda <foronda.reagan@gmail.com>
2018-10-05 15:07:41 -0600

    add dashboard header

---
+++ src/components/Dashboard/DashboardHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function DeashboardHeader(props) {
+    return (
+        <div className='dashboardheader'>
+            Dashboard Header
+        </div>
+    )
+}
