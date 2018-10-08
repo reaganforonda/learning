4605fea620861b6c3a8dc3d50a4c2068503cdaaa
reaganforonda <foronda.reagan@gmail.com>
2018-10-08 14:53:38 -0600

    added user reducer and class reducer

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const INITIAL_STATE = {
+
+}
+
+export default function userReducer(state=INITIAL_STATE, action) {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
