81871c53f75686c8ec24da6c322841b18a7ef2ea
reaganforonda <foronda.reagan@gmail.com>
2018-10-08 20:26:13 -0600

    add load classes for user

--- src/ducks/classReducer.js
+++ src/ducks/classReducer.js
@@ -1,11 +1,21 @@
-import axios from 'axios';
-
 const INITIAL_STATE = {
+    classes: ''
+}
 
+const LOAD_ALL_CLASSES = "LOAD_ALL_CLASSES";
+
+export function loadUserClasses(classes) {
+    return {
+        type: LOAD_ALL_CLASSES,
+        payload: classes
+    }
 }
 
 export default function classReducer(state=INITIAL_STATE, action) {
     switch(action.type) {
+        case LOAD_ALL_CLASSES:
+            return Object.assign({}, state, {classes: action.payload});
+
         default:
             return state
     }
