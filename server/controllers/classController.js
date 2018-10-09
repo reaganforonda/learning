47d1e7d35c3d629371c7d506dacde4df1e1893ff
reaganforonda <foronda.reagan@gmail.com>
2018-10-09 00:25:47 -0600

    added getclasses

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -1,7 +1,20 @@
 module.exports = {
-    create: (req, res) => {
+    createClass: (req, res) => {
         const db = req.app.get('db');
 
         
-    }
+    },
+
+    getClasses: (req, res) => {
+        const db = req.app.get('db');
+
+        const {userID} = req.query;
+        
+        db.GET_USER_CLASSES([userID]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to retrive user classes: ${err}`);
+            res.sendStatus(500);
+        })
+    },
 }
