14ea5ef55f7694f3df99327e76f9ba018047c314
reaganforonda <foronda.reagan@gmail.com>
2018-10-08 19:00:25 -0600

    add load user

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 
 const INITIAL_STATE = {
+    user: ''
+}
 
+const SET_ACTIVE_USER = "SET_ACTIVE_USER";
+
+export function loadUser(user) {
+    return {
+        type: SET_ACTIVE_USER,
+        payload: user
+    }
 }
 
 export default function userReducer(state=INITIAL_STATE, action) {
     switch(action.type) {
+        case SET_ACTIVE_USER:
+            return Object.assign({}, state, {user: action.payload});
+        
         default:
             return state
     }
