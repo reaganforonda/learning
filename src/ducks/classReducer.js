4cf60e4a849ebea463374de9c905f89f7e0467e4
reaganforonda <foronda.reagan@gmail.com>
2018-10-09 00:06:47 -0600

    changed the load classes function

--- src/ducks/classReducer.js
+++ src/ducks/classReducer.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
+
 const INITIAL_STATE = {
-    classes: ''
+    classes: [],
+    loading: true
 }
 
 const LOAD_ALL_CLASSES = "LOAD_ALL_CLASSES";
 
-export function loadUserClasses(classes) {
+export function loadUserClasses(user) {
+    let classes = axios.get(`/api/classes?userID=${user.user_id}`).then((result) => {
+        return result.data
+    });
+
     return {
         type: LOAD_ALL_CLASSES,
         payload: classes
@@ -13,8 +20,12 @@ export function loadUserClasses(classes) {
 
 export default function classReducer(state=INITIAL_STATE, action) {
     switch(action.type) {
-        case LOAD_ALL_CLASSES:
-            return Object.assign({}, state, {classes: action.payload});
+
+        case LOAD_ALL_CLASSES + "_PENDING":
+            return Object.assign({}, state, {loading: true});
+
+        case LOAD_ALL_CLASSES + "_FULFILLED":
+            return Object.assign({}, state, {classes: action.payload, loading: false});
 
         default:
             return state
