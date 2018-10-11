085e7d8c9fca42b0f7202fb2adb3022d81c83378
reaganforonda <foronda.reagan@gmail.com>
2018-10-10 21:14:57 -0600

    createclass function

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -1,8 +1,15 @@
 module.exports = {
     createClass: (req, res) => {
         const db = req.app.get('db');
+        const {className, classCode, schoolName, schoolYear, classStartDate, classEndDate} = req.body;
+        const {userID} = req.query
 
-        
+        db.CREATE_CLASS([userID, className, schoolYear, classCode, schoolName, classStartDate, classEndDate]).then(() => {
+            res.sendStatus(200);
+        }).catch((err) => {
+            console.log(`Server error while attempting to add new class: ${err}`);
+            res.sendStatus(500);
+        })
     },
 
     getClasses: (req, res) => {
