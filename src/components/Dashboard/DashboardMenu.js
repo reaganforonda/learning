8042a0e6cec87a0eb0db09bda0f2cb65adeedd6f
reaganforonda <foronda.reagan@gmail.com>
2018-10-10 21:21:10 -0600

    added placeholader navlinks

--- src/components/Dashboard/DashboardMenu.js
+++ src/components/Dashboard/DashboardMenu.js
@@ -12,8 +12,10 @@ export default class DasbhoardMenu extends React.Component{
         return (
             <div className='dashboardmenu'>
                 <nav className='nav-menu'>
-                    <NavLink className='side-link' activeClassName='active-link' exact to='/dasbhoard/classes'>Classes</NavLink>
-                    
+                    <NavLink className='side-link' activeClassName='active-link' exact to='/dashboard/classes'>Classes</NavLink>
+                    <NavLink className='side-link' activeClassName='active-link' exact to='/dashboard/attendance'>Attendance</NavLink>
+                    <NavLink className='side-link' activeClassName='active-link' exact to='/dashboard/grading'>Grading</NavLink>
+                    <NavLink className='side-link' activeClassName='active-link' exact to='/dashboard/students'>Students</NavLink>
                 </nav>
             </div>
         )
