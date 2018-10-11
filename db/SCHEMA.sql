a8d897c4340b228370fe3fa1832a31988ce68630
reaganforonda <foronda.reagan@gmail.com>
2018-10-10 21:14:22 -0600

    added another column to classes table

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -13,7 +13,9 @@ CREATE TABLE classes (
     class_id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(user_id),
     class_name VARCHAR(45),
+    school_year VARCHAR(45),
     class_code VARCHAR(45),
     school_name VARCHAR(45),
-    class_time DATE
+    class_start_date DATE,
+    class_end_date DATE
 );
