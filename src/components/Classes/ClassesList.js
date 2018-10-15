ce7e2a11a8250e6c03ac8dd7188cbc73927d22f3
reaganforonda <foronda.reagan@gmail.com>
2018-10-15 14:51:34 -0600

    added labels

--- src/components/Classes/ClassesList.js
+++ src/components/Classes/ClassesList.js
@@ -4,9 +4,9 @@ export default function ClassesList(props) {
     let classes = props.classes.map((value, index)=> {
         return (
             <div className='teacher-class' key={value.classe_id} onClick={(e)=>props.setActiveClass(e, value)}>
-                <div>{value.class_name}</div>
-                <div>{value.class_code}</div>
-                <div>{value.school_name }</div>
+                <div>CLASS NAME: {value.class_name}</div>
+                <div>CLASS CODE: {value.class_code}</div>
+                <div>SCHOOL NAME: {value.school_name }</div>
             </div>
         )
     })
