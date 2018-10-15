bbaf7e919067ed5de6e6bfdaf9e730013d48ff37
reaganforonda <foronda.reagan@gmail.com>
2018-10-15 09:14:40 -0600

    work on displaying list of classes

--- src/components/Classes/ClassesList.js
+++ src/components/Classes/ClassesList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default function ClassesList(props) {
+    let classes = props.classes.map((value, index)=> {
+        return (
+            <div className='teacher-class' key={value.classe_id} onClick={(e)=>props.setActiveClass(e, value)}>
+                <div>{value.class_name}</div>
+                <div>{value.class_code}</div>
+                <div>{value.school_name }</div>
+            </div>
+        )
+    })
+
     return(
         <div className='classes-list'>
-            Classes List
+            {classes}
         </div>
     )
 }
