b85fd7fe8f7f59df45651600c49f2b712627e8d1
reaganforonda <foronda.reagan@gmail.com>
2018-10-16 16:10:02 -0600

    added redux

--- src/components/CourseWork/CourseWorkView.js
+++ src/components/CourseWork/CourseWorkView.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from '../Header/Header';
+import AddCourseWorkForm from './AddCourseWorkForm';
+import {connect} from 'react-redux';
 
-export default class CourseWorkView extends React.Component{
+
+export class CourseWorkView extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state={}
+        this.state={
+            displayForm: false
+        }
+    }
+
+    toggleDisplayForm(){
+        if(this.state.displayForm) {
+            this.setState({displayForm: false});
+        } else {
+            this.setState({displayForm: true});
+        }
     }
 
     render(){
@@ -13,9 +26,21 @@ export default class CourseWorkView extends React.Component{
             <div className='course-work-view'>
                 <Header headerTitle={'Course Work'}/>
                 <div>
-                    Course Work View
+                    <div><button onClick={()=>this.toggleDisplayForm()}>Add Course Work</button></div>
+                    {
+                        this.state.displayForm ? <div className='form-modal'><AddCourseWorkForm/> </div>: null
+                    }
                 </div>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user,
+        activeClass: state.classReducer.activeClass
+    }
+}
+
+export default connect(mapStateToProps, {})(CourseWorkView);
