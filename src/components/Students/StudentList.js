f130a67a52d5069d3c40f9cabe9ea94ac66800b3
reaganforonda <foronda.reagan@gmail.com>
2018-10-16 14:56:02 -0600

    fix typo in props.map

--- src/components/Students/StudentList.js
+++ src/components/Students/StudentList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function StudentList(props) {
-    let students = props.classes.map((student, index) => {
+    let students = props.students.map((student, index) => {
         return (
             <div className='student' key={student.student_id}>
                 <div>Student First Name: {student.first_name}</div>
