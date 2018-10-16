d74f6021ba01f44fbe0705712d937d47ef50d88e
reaganforonda <foronda.reagan@gmail.com>
2018-10-16 10:29:59 -0600

    create studentview

---
+++ src/components/Students/StudentsView.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'redux';
+import Header from '../Header/Header';
+
+export class StudentsView extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div className='students-view'>
+                <Header headerTitle={'Students'}/>
+                <main>
+                    Student view
+                </main>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user
+    }
+}
+
+export default connect(mapStateToProps, {})(withRouter(StudentsView));
