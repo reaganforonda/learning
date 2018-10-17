8088d5842d7c721fa51c0eb209c732d2402eee22
reaganforonda <foronda.reagan@gmail.com>
2018-10-17 15:05:22 -0600

    create attendance reducer

---
+++ src/ducks/attendanceReducer.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const INITIAL_STATE = {
+
+}
+
+
+export default function attendanceReducer(state=INITIAL_STATE, action) {
+    switch(action.type) {
+
+        default:
+            return state
+    }
+}
