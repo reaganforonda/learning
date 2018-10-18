b70df839b72a19d7235d300c7ea8cde5ebd39427
reaganforonda <foronda.reagan@gmail.com>
2018-10-17 22:29:10 -0600

    add get attedance function

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -1,10 +1,22 @@
 module.exports = {
     addAttendance: (req, res)=> {
+        const db = req.app.get('db');
+        const {user_id, class_id, student_id} = req.body;
+
+        
 
     },
 
     getAttendance: (req, res)=> {
+        const db = req.app.get('db');
+        const {user_id, class_id, student_id} = req.query;
 
+        db.GET_ATTENDANCE([user_id, class_id, student_id]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to retrieve attendance: ${err}`);
+            res.sendStatus(500);
+        })
     },
 
 }
